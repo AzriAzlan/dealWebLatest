ea565f81909b2ea26e8b0aa37fe48d92095333d4
AzriAzlan <81690393+AzriAzlan@users.noreply.github.com>
2021-07-27 18:50:20 +0800

    Has to be logged in to use

---
+++ dealshare/landingpage.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <!-- bootstrap 4.1.1 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-lg-5" style="border-right:solid 1px;">
+            <img src="savedDeal/Icon/frame.png" style="width:50%; height:50%">
+        </div>
+        <div class="col-lg-5" style="text-align:center">
+            <h1>Here's your "Name" coupon</h1>
+            <p>Use the following promocode at "Link" to get "Reward" on your purchase</p>
+            <p>Promocode</p>
+            <button>TAKE ME TO STORE</button>
+        </div>
+    </div>
+</body>
+
+</html>
